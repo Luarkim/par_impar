52df25ab9d420916d330c64c0ef0eb9f1bfc0d1a
luarkim <parkleeh31@gmail.com>
2022-04-08 21:34:32 -0300

    tarefa realizada

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int numero;
+string paridade;
+
+Console.Write("digite um núumero: ");
+numero = Convert.ToInt32(Console.ReadLine ());
+
+paridade = (numero % 2) == 0 ? "par" : "ímpar";
+
+Console.WriteLine($"{numero} é {paridade}");
